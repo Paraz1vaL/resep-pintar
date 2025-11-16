407055d1481a756bcabcc8a3f63b80c62f671672
Paraz1vaL <hudanmadani08@gmail.com>
2025-11-16 18:34:49 +0700

    Mengganti translator ke DeepL

--- tentang.py
+++ tentang.py
@@ -11,7 +11,7 @@ def show_page():
     ### Teknologi yang Digunakan
     * **Framework:** Streamlit
     * **API Resep:** Spoonacular
-    * **Penerjemah:** deep-translator (via Google Translate)
+    * **Penerjemah:** DeepL
     """)
     st.markdown("---")
    
